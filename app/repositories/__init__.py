4a22e43256e33239c783f1b4deef78ac780beb27
crosik52 <topatletpro@gmail.com>
2026-07-05 19:32:00 +0500

    Add app/repositories/__init__.py

---
+++ app/repositories/__init__.py
@@ -0,0 +1,5 @@
+from app.repositories.message_repository import MessageRepository
+from app.repositories.stats_repository import StatsRepository
+from app.repositories.user_repository import UserRepository
+
+__all__ = ["MessageRepository", "StatsRepository", "UserRepository"]

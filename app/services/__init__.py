91ca81dbbea5f6eea0f6696be0deecc8ecc83f2e
crosik52 <topatletpro@gmail.com>
2026-07-05 19:32:10 +0500

    Add app/services/__init__.py

---
+++ app/services/__init__.py
@@ -0,0 +1,6 @@
+from app.services.auth_service import AuthService
+from app.services.export_service import ExportService
+from app.services.message_service import MessageService
+from app.services.stats_service import StatsService
+
+__all__ = ["AuthService", "ExportService", "MessageService", "StatsService"]

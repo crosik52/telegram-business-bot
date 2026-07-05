db6ba4a1637005cdf31eba54dd3c52da4cfee2b1
crosik52 <topatletpro@gmail.com>
2026-07-05 19:31:56 +0500

    Add app/models/__init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,10 @@
+from app.models.business_connection import BusinessConnection
+from app.models.message import Message, MessageEditHistory
+from app.models.user import TelegramUser
+
+__all__ = [
+    "BusinessConnection",
+    "Message",
+    "MessageEditHistory",
+    "TelegramUser",
+]
